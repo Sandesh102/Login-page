b2b1add90072a75bb3997dc0700261e729d762c8
Sandesh102 <104638658+Sandesh102@users.noreply.github.com>
2023-06-27 18:57:34 +0545

    Update script.js
    Simple comments deletion

--- script.js
+++ script.js
@@ -1,14 +1,9 @@
 document.getElementById("loginForm").addEventListener("submit", function(event) {
-  event.preventDefault(); // Prevent form submission
-
-  // Get input values
+  event.preventDefault();
   var username = document.getElementById("username").value;
   var password = document.getElementById("password").value;
-
-  // Check if username and password are valid (dummy validation)
   if (username === "admin" && password === "password") {
     alert("Login successful!");
-    // Redirect to the hotel management page or perform any other action
   } else {
     alert("Invalid username or password. Please try again.");
   }
